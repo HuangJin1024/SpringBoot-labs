d370d2a060dfddcb122e8856b599907ff49a282d
HuangJin1024 <48778619+HuangJin1024@users.noreply.github.com>
2020-06-19 16:27:58 +0800

    Update AdminController.java

--- lab-01/lab-01-springsecurity-demo/src/main/java/cn/iocoder/springboot/lab01/springsecurity/controller/AdminController.java
+++ lab-01/lab-01-springsecurity-demo/src/main/java/cn/iocoder/springboot/lab01/springsecurity/controller/AdminController.java
@@ -10,7 +10,7 @@ public class AdminController {
 
     @GetMapping("/demo")
     public String demo() {
-        System.out.println("test");
+        System.out.println("test001");
         return "示例返回";
     }
 
